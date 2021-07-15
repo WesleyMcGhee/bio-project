d04e68de27f0d8230fb18b4de8f9366d754db28c
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-15 14:04:09 -0500

    test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use('/static', express.static(path.join(__dirname, '../public')))
+app.use(express.static("public"));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, 'public'));
 })
 
 const port = process.env.PORT || 4004;
